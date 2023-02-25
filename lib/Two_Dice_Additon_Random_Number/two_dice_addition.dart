32433d0f717c96e45ae2633b7c07b4e2a96ec651
Vivek <tailorvivekgec19@gmail.com>
2023-02-25 01:30:24 -0800

    DICE

--- lib/Two_Dice_Additon_Random_Number/two_dice_addition.dart
+++ lib/Two_Dice_Additon_Random_Number/two_dice_addition.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -25,62 +25,69 @@ int add = 0;
 class _Two_Dice_AdditionState extends State<Two_Dice_Addition> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black,
-      appBar: AppBar(backgroundColor: Colors.teal.shade800,
-        title: Text(
-          "Addition of Two Dice",
-          style: TextStyle(fontSize: 30),),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 20,),
-            Container(
-              height: 300,
-              width: 600,
-              color: Colors.pinkAccent,
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:
-                [
-                   Container(height: 250,width: 250,
-                     child: Image.asset(
-                    "${photo[i]}",
-                     height: 180, width: 180,
-                  ),),
-
-                  Container(height: 250,width: 250,
-                    child: Image.asset(
-                      "${photo[j]}",
-                      height: 180, width: 180,
+    return SafeArea(
+      child: Scaffold(backgroundColor: Colors.black,
+        appBar: AppBar(backgroundColor: Colors.teal.shade800,
+          title: Text(
+            "Addition of Two Dice",
+            style: TextStyle(fontSize: 30),),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20,),
+              Container(
+                height: 300,
+                width: 600,
+                color: Colors.pinkAccent,
+                alignment: Alignment.center,
+                child: Row(
+                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children:
+                  [
+                     Expanded(
+                       child: Container(height: 100,width: 100,
+                         child: Image.asset(
+                        "${photo[i]}",
+                         height: 100, width: 100,
                     ),),
-                ],
-              ),), //  Container for two Dice
-            SizedBox(height: 20,),
-            ElevatedButton(onPressed: (){
+                     ),
 
-              Random rnd = Random();
-              int z = rnd.nextInt(6);
-              int y = rnd.nextInt(6);
-              setState(() {
-                i = z;
-                j = y;
-                add = z + y + 2;
+                    Expanded(
+                      child: Container(height: 100,width: 100,
+                        child: Image.asset(
+                          "${photo[j]}",
+                          height: 100, width: 100,
+                        ),),
+                    ),
+                  ],
+                ),), //  Container for two Dice
+              SizedBox(height: 20,),
+              ElevatedButton(onPressed: (){
 
-              });
-            }, child: Text("Press",style: TextStyle(fontSize: 20),)),
-            SizedBox(height: 20,),
-            Container(
-              height: 50,
-              width: 420,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(color: Colors.limeAccent,borderRadius: BorderRadius.all(Radius.circular(20))),
-              child: Text("Additon of Both Matrix Number is  ${add}",style: TextStyle(fontSize: 20)),
-            )
+                Random rnd = Random();
+                int z = rnd.nextInt(6);
+                int y = rnd.nextInt(6);
+                setState(() {
+                  i = z;
+                  j = y;
+                  add = z + y + 2;
 
-          ],
+                });
+              }, child: Text("Press",style: TextStyle(fontSize: 20),)),
+              SizedBox(height: 20,),
+              Container(
+                height: 50,
+                width: 420,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(color: Colors.limeAccent,borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Text("Additon of Both Matrix Number is  ${add}",style: TextStyle(fontSize: 20)),
+              )
+
+            ],
+          ),
         ),
       ),
     );
